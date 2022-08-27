2f8980039a0c55d248f92af2063777cdd0e32b7e
Pillifino <dinodizon073100@gmail.com>
2022-08-27 11:49:25 -0700

    image is working!

--- my-app/src/Sections/Home/home.tsx
+++ my-app/src/Sections/Home/home.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 
 // Chakra
+import { Image } from '@chakra-ui/react'
+import Background from "../../Assets/Images/background.jpg";
 
 function Home() {
   return (
     <div>
+        <Image 
+        src={Background}
+        fit="fill" 
+        alt='svg image'/>
     </div>
   );
 }
