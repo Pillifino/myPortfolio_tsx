b218a676a1a41708594d419504b4f3cb6155d883
Pillifino <dinodizon073100@gmail.com>
2022-08-29 10:49:09 -0700

    layout styling

--- my-app/src/Sections/Projects/projects.tsx
+++ my-app/src/Sections/Projects/projects.tsx
@@ -1,9 +1,23 @@
+import { Container, Flex } from '@chakra-ui/react'
 import React from 'react'
 
 function Projects() {
   return (
     <div>
-      
+      <Flex
+        bg="black"
+        mt="60vh"
+        w="100vw"
+        height="90vh"
+      >
+        <Container
+          // bg="white"
+          height="100%"
+          maxWidth="80%"
+        >
+
+        </Container>
+      </Flex>
     </div>
   )
 }
