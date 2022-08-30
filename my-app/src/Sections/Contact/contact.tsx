6be60a6a24debffe91cb43ad986c593be13f95dc
Pillifino <dinodizon073100@gmail.com>
2022-08-30 10:13:15 -0700

    from layout and project layout

--- my-app/src/Sections/Contact/contact.tsx
+++ my-app/src/Sections/Contact/contact.tsx
@@ -2,24 +2,96 @@ import React from 'react'
 
 import ContactBackground from "../../Assets/Images/contact.jpg"
 
-import { Flex, Image } from "@chakra-ui/react"
+import { Flex, Image, FormControl, FormLabel, FormErrorMessage, FormHelperText, Input, Text } from "@chakra-ui/react"
 
 function Contact() {
   return (
     <div>
       <Flex
-      bg="white"
-      h="100vh"
-      w="100vw"
+        bg="white"
+        h="100vh"
+        w="100vw"
+        position="relative"
+        justify="center"
       >
         <Image
-        src={ContactBackground}
-        boxSize="xl"
-        objectFit="cover"
-        h="100%"
-        alt='svg image'
-        w="100%"
+          src={ContactBackground}
+          boxSize="xl"
+          objectFit="cover"
+          h="100%"
+          alt='svg image'
+          w="100%"
         />
+        <Flex
+          // bg="green"
+          flexDirection="column"
+          position="absolute"
+          justify="center"
+          alignItems="center"
+          h="100%"
+          w="80%"
+        >
+          <Text
+          // bg="blue"
+          w="100%"
+          fontSize="3xl"
+          color="white"
+          >
+            Contact Me
+          </Text>
+          {/* Form */}
+          <FormControl
+            // bg="red"
+            h="60%"
+          >
+            <Input
+              color="white"
+              placeholder="First Name"
+              bg="black"
+              opacity=".5"
+              w="50%"
+              h="20%"
+              borderRadius="20px"
+            />
+            <Input
+              color="white"
+              placeholder="Last Name"
+              bg="black"
+              opacity=".5"
+              w="50%"
+              h="20%"
+              borderRadius="20px"
+            />
+            <Input
+              color="white"
+              placeholder="Last Name"
+              type='email'
+              bg="black"
+              opacity=".5"
+              w="50%"
+              h="20%"
+              borderRadius="20px"
+            />
+            <Input
+              color="white"
+              placeholder="Phone Number"
+              bg="black"
+              opacity=".5"
+              w="50%"
+              h="20%"
+              borderRadius="20px"
+            />
+            <Input
+              color="white"
+              placeholder="Message"
+              bg="black"
+              opacity=".5"
+              w="100%"
+              h="60%"
+              borderRadius="20px"
+            />
+          </FormControl>
+        </Flex>
       </Flex>
     </div>
   )
--- my-app/src/Sections/Projects/projects.tsx
+++ my-app/src/Sections/Projects/projects.tsx
@@ -1,5 +1,7 @@
-import { Container, Flex } from '@chakra-ui/react'
+import { Container, Flex, Image, Text } from '@chakra-ui/react'
 import React from 'react'
+
+import images from '../../images'
 
 function Projects() {
   return (
@@ -8,13 +10,36 @@ function Projects() {
         bg="black"
         w="100vw"
         height="90vh"
+        justify="center"
+        alignItems="center"
       >
         <Container
-          bg="white"
-          height="100%"
+          // bg="white"
+          display="flex"
+          flexDirection="row"
+          flexWrap="wrap"
+          justifyContent="space-between"
+          alignItems="center"
+          height="60%"
           maxWidth="80%"
+          position="relative"
         >
-
+          {images.map((image)=> {
+            return (
+              <Image 
+              src={image}
+              w="45%"
+              h="45%"
+              borderRadius="25px"
+              filter='auto' 
+              blur='2px'
+              opacity=".8"
+              _hover={{ blur: "0px", opacity:"1"}}
+              transition=".8s"
+              >
+              </Image>
+            );
+          })}
         </Container>
       </Flex>
     </div>
